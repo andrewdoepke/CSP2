9b1abc6f355e41b723ceebade5039444a616bf0c
Andrew Doepke <andrewdoepke@outlook.com>
2020-12-06 23:36:43 -0600

    Did some things
    Moved stuff to static and figured out all of our main app routes for now

--- main.py
+++ main.py
@@ -4,8 +4,34 @@ app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/home')
 def main():
     return render_template('homepage.html')
+
+
+@app.route('/getmc')
+def getmc():
+    return render_template('get-minecraft.html')
+
+
+@app.route('/blogpost', methods=['GET', 'POST'])
+def blog():
+    return render_template('blog-posts.html')
+
+
+@app.route('/worldseed', methods=['GET', 'POST'])
+def ws():
+    return render_template('world-seeds.html')
+
+
+@app.route('/buildideas', methods=['GET', 'POST'])
+def bi():
+    return render_template('build-ideas.html')
+
+
+@app.route('/adminlogin', methods=['GET', 'POST'])
+def login():
+    return render_template('admin-login.html')
 
 
 if __name__ == "__main__":
