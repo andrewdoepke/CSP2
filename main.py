51103aeffa7f37e4da5c64ad6c382d5f96b21c34
Andrew Doepke <andrewdoepke@outlook.com>
2020-12-08 13:02:36 -0600

    404 Page Not Found
    Now we have a handler for this!

--- main.py
+++ main.py
@@ -34,5 +34,10 @@ def login():
     return render_template('admin-login.html')
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
